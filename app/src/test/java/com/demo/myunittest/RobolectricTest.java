084e899c1dd45b2004814c64053a2dcf3c0345f1
soapgu <ghost_gu@163.com>
2022-12-08 23:30:16 +0800

    test: 💍 增加Robolectric和Mockito混合测试用例

--- app/src/test/java/com/demo/myunittest/RobolectricTest.java
+++ app/src/test/java/com/demo/myunittest/RobolectricTest.java
@@ -1,14 +1,28 @@
 package com.demo.myunittest;
 
+import android.os.Looper;
 import android.widget.TextView;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+import static org.robolectric.Shadows.shadowOf;
+
+import com.demo.myunittest.model.UuidResponse;
+import com.demo.myunittest.util.HttpClientWrapper;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockedStatic;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.android.controller.ActivityController;
+
+import java.util.UUID;
+
+import io.reactivex.rxjava3.core.Single;
 
 @RunWith(RobolectricTestRunner.class)
 public class RobolectricTest {
@@ -20,6 +34,28 @@ public class RobolectricTest {
             MainActivity activity = controller.get();
             TextView textView = activity.findViewById(R.id.tv_message);
             assertEquals( "Hello World!",textView.getText() );
+        }
+    }
+
+    @Test
+    public void testClickAndMockResponse(){
+        try (ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class)) {
+            controller.setup();
+            try( MockedStatic<HttpClientWrapper> mocked = mockStatic(HttpClientWrapper.class) ) {
+                HttpClientWrapper instance = mock(HttpClientWrapper.class);
+                UuidResponse response = new UuidResponse();
+                UUID uuid = UUID.randomUUID();
+                response.setUuid(uuid.toString());
+                when(instance.ResponseJson(any(), any()))
+                        .thenReturn(Single.just(response));
+                mocked.when(HttpClientWrapper::getInstance)
+                        .thenReturn(instance);
+                MainActivity activity = controller.get();
+                TextView textView = activity.findViewById(R.id.tv_message);
+                activity.findViewById(R.id.button_test).performClick();
+                shadowOf(Looper.getMainLooper()).idle();
+                assertEquals( uuid.toString(),textView.getText() );
+            }
         }
     }
 }
